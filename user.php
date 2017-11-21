41330dbd75db9fcb0e7e158189a52131db241633
Chris Grabosky <chris@grabosky.net>
2017-11-21 14:43:47 -0500

    Basic user mgmt

---
+++ user.php
@@ -0,0 +1,94 @@
+<?php
+@session_start();
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+    
+if(!isset($_SESSION['un']) || !isset($_SESSION['uid'])) { header('LOCATION: login.php'); }
+require_once('includes/util.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <link rel="icon" href="/favicon/favicon.ico">
+    <link rel="apple-touch-icon" sizes="57x57" href="/favicon/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="/favicon/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="/favicon/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="/favicon/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="/favicon/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="/favicon/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="/favicon/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="/favicon/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="/favicon/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="/favicon/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/favicon//manifest.json">
+    <meta name="msapplication-TileColor" content="#<?php echo THEMECOLOR;?>">
+    <meta name="msapplication-TileImage" content="/favicon/ms-icon-144x144.png">
+    <meta name="theme-color" content="#<?php echo THEMECOLOR;?>" />
+
+    <title>User Management for <?php echo APPNAME;?></title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/signin.css" rel="stylesheet">
+    
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <div class="container">
+
+      <form class="form-signin" method="post" action="c_user.php">
+        <h2 class="form-signin-heading">Create a User</h2>
+
+        <label for="user" class="sr-only">User</label>
+        <input type="text" id="usermsg" name="user" class="form-control" placeholder="user" required autofocus>
+
+        <label for="passcode" class="sr-only">Passcode</label>
+        <input type="text" id="passcode" name="passcode" class="form-control" placeholder="passcode" equired>
+        
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Create</button>
+
+        <h2 class="form-signin-heading">Current Users</h2>
+
+        <table class="table table-striped">
+        <thead><tr><th>Username</th><th>Last Activity</th></tr></thead>
+        <tbody>
+        <?php
+        $conn = connectDB();
+        $sql = "SELECT username, passcode, lastactivity from user";
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+        $result = $stmt->fetchAll();
+        $mediaList = array();
+        foreach($result as $r)
+        {
+          echo "<tr><td>".$r['username']."</td><td>".$r['lastactivity']."</td></tr>";
+        }
+        ?>
+        </tbody></table>
+      </form>
+
+    </div> <!-- /container -->
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
