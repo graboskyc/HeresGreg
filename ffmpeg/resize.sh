62704e3ded34525a2b7edd52990b4092da68e298
Chris Grabosky <chris@grabosky.net>
2021-07-06 12:33:02 -0400

    fix bad partial encode

--- ffmpeg/resize.sh
+++ ffmpeg/resize.sh
@@ -5,8 +5,7 @@ bigFiles=(/app/wwwroot/media/*.mp4)
 for file in ${bigFiles[*]}
 do 
 	filename="${file##*/}"
-	if [ ! -f /app/wwwroot/media/smaller/$filename ]
-	then 
+	if [ ! -f /app/wwwroot/media/smaller/$filename ]; then 
 		echo $filename
 		cp /app/wwwroot/media/$filename /app/wwwroot/media/bu/$filename
 		/ffmpeg/ffmpeg -i /app/wwwroot/media/$filename -vf scale="640:trunc(ow/a/2)*2" -map 0:v? -map 0:a? -map 0:s? -c:v libx264 -movflags faststart /app/wwwroot/media/smaller/$filename
@@ -14,6 +13,8 @@ do
 		echo $lastCmd
 		if [ $lastCmd -eq 0 ]; then
 			rm -f /app/wwwroot/media/$filename
+		else
+			rm -f /app/wwwroot/media/smaller/$filename
 		fi
 	fi
 done
