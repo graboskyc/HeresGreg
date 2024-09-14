1a38f9fb406e21073d9308bec440a739d4881173
Chris Grabosky <chris@grabosky.net>
2024-09-14 12:37:53 -0400

    remove app services bu

--- RealmExport/functions/isAdmin.js
+++
@@ -1,8 +0,0 @@
-exports = function(user){
-  const validAdmins = context.values.get("validAdmins");
-  if (validAdmins.indexOf(user.id) > -1) {
-    return true;
-  } else {
-    return false;
-  }
-};
